01f847a1cd3fedfb5c3a061f6d95e01b39a46e1f
Sankalp Goyal <154608321+Sankalp-1711@users.noreply.github.com>
2024-09-26 18:44:22 +0530

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Footer.scss';
 import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
-
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap');
 </style>
@@ -10,16 +9,18 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__top">
-        <div className="footer-start">
-          <div className="footer__logo">
-            <div className='lg' style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-              <h2 style={{ fontSize: '40px', margin: '17px 0px 0px 0px' }} className="footer_logo_heading">Pritam Nayak</h2>
-              <p style={{ padding: '0', margin: '0' }}>COUTURE</p>
-            </div>
-            <p>Don't just buy an outfit. Buy an identity.</p>
+    <div className='footer-start'>
+        <div className="footer__logo">
+          <div style={{display:'flex',alignItems:'center',flexDirection:"column"}}>
+        
+          <h2 style={{ fontSize: '40px',margin:'17px 0px 0px 0px' }} className='footer_logo_heading' >Pritam Nayak</h2>
+          <p style={{padding:'0',margin:'0'}}>COUTURE</p>
           </div>
+         
+          <p>Don't just buy an outfit. Buy an identity.</p>
+        </div>
 
-<div className='footer-col'>
+       
           <div className="footer__section">
             <h3>Company</h3>
             <ul>
@@ -40,7 +41,6 @@ const Footer = () => {
               <li>Shipping</li>
             </ul>
           </div>
-          </div>
 
           <div className="footer__section">
             <h3>Store Locator</h3>
@@ -51,9 +51,17 @@ const Footer = () => {
           </div>
         </div>
       </div>
+     
+      
+      
 
       <div className="footer__bottom">
         <p>© Copyright 2024. All Rights Reserved by Pritam Nayak Couture.</p>
+        <div className="footer-social">
+          <FaFacebookF className='social_logo' />
+          <FaInstagram className='social_logo' />
+          <FaTwitter className='social_logo' />
+        </div>
       </div>
     </footer>
   );
